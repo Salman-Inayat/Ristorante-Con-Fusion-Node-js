b41b4d2cb8f41d40ee93c1934e85d7a383f86e12
Salman-Inayat <salmaninayat1999@gmail.com>
2020-07-04 22:45:48 +0500

    Passport JWT authentication added

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ const Promotions = require("./models/promotions");
 const Leaders = require('./models/leaders');
 var passport = require('passport');
 var authenticate = require('./authenticate');
+var config = require("./config");
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url, {useNewUrlParser: true });
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
@@ -33,37 +34,16 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}))
-
 app.use(passport.initialize());
 app.use(passport.session());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 
-function auth(req, res, next) {
-  console.log(req.user);
-  if(!req.user){
-      var err = new Error('You are not authenticated');
-      err.status = 403;
-      next(err);
-    }
-  else{
-   next();
-  } 
-}
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser("12345-67890-09876-54321"));
-app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
--- routes/promoRouter.js
+++ routes/promoRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const Promotions = require("../models/promotions");
-
+var authenticate = require('../authenticate');
 const promoRouter = express.Router();
 
 promoRouter.use(bodyParser.json());
@@ -17,7 +17,7 @@ promoRouter.route("/")
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post((req, res, next) => {
+.post(authenticate.verifyUser, (req, res, next) => {
     Promotions.create(req.body)
     .then((promo) => {
         console.log('Promotion Created ', promo);
@@ -27,11 +27,11 @@ promoRouter.route("/")
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.put((req, res, next) => {
+.put(authenticate.verifyUser, (req, res, next) => {
 res.statusCode = 403;
 res.end("PUT operation not supported on /promotions");
 })
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
     Promotions.remove({})
     .then((resp) => {
         res.statusCode = 200;
@@ -45,17 +45,17 @@ promoRouter.route("/:promoId")
 .get((req, res, next) => {
     Promotions.findById(req.params.promoId)
     .then((promo) => {
-        res.statusCode = 200;
+        res.statusCode = 200;``
         res.setHeader('Content-Type', 'application/json');
         res.json(promo);
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post((req, res, next) => {
+.post(authenticate.verifyUser, (req, res, next) => {
 res.statusCode = 403;
 res.end("POST operation not supported on /promotions/" + req.params.promoId);
 })
-.put((req, res, next) => {
+.put(authenticate.verifyUser, (req, res, next) => {
     Promotions.findByIdAndUpdate(req.params.promoId, {
         $set: req.body
     }, { new: true })
@@ -66,7 +66,7 @@ res.end("POST operation not supported on /promotions/" + req.params.promoId);
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
     Promotions.findByIdAndRemove(req.params.promoId)
     .then((resp) => {
         res.statusCode = 200;
